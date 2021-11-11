93ba0767b1e84b55f4bfed9731fb61e05c5521ce
Fabio Oliveira Costa <fabiocostadev@gmail.com>
2021-11-11 15:55:51 +0100

    changes icon, simplifies code

--- newTab.js
+++ newTab.js
@@ -2,22 +2,14 @@ const NUM_COLORS = 16;
 
 (function () {
   const onLoad = async () => {
-    const wordsUrl = chrome.runtime.getURL("assets/words.json");
-
-    const tablesUrl = chrome.runtime.getURL("assets/tables.json");
-
-    // TODO move to workers or something
-    let [wordsData, tablesData] = await Promise.all([
-      fetch(wordsUrl),
-      fetch(tablesUrl),
-    ]);
-
-    wordsData = await wordsData.json();
-    tablesData = await tablesData.json();
-    console.log({ tablesData });
-    const sources = [wordsData, tablesData];
+    /**
+     * Get the datas
+     */
+    const sources = await getDataSources();
+    /**
+     *Chooses the data soruce to use
+     */
     const sourceToUse = Math.round(Math.random() * (sources.length - 1));
-    console.log({ sourceToUse });
     renderDataSource(sources[sourceToUse]);
   };
   onLoad();
@@ -26,70 +18,97 @@ const NUM_COLORS = 16;
 function renderDataSource(source) {
   const wordContainer = document.getElementById("wordContainer");
   const tableContainer = document.getElementById("tableContainer");
-  const isTable = Array.isArray(source); // Todo make a better data packed
+  const isTable = Array.isArray(source); // Todo make a better data packet
   wordContainer.classList.add("hidden");
   tableContainer.classList.add("hidden");
-  let colorIndex;
   if (isTable) {
-    const numTables = source.length;
-    const tableOfTheTab = Math.floor(Math.random() * numTables);
-    colorIndex = (tableOfTheTab % NUM_COLORS) + 1;
-    const { title, rows } = source[tableOfTheTab];
-
-    const makeRows = (dataRows) => {
-      const stringifiedRows = [];
-
-      dataRows.forEach((data) => {
-        if (Array.isArray(data)) {
-          stringifiedRows.push(data.map((d) => `<td>${d}</td>`).join(""));
-        } else {
-          if (data.hasOwnProperty("header")) {
-            stringifiedRows.push(
-              data.header
-                .map((d) => {
-                  let content = d;
-                  let props = "";
-                  if (typeof content !== "string") {
-                    content = d.text;
-                    Object.entries(d).forEach(([key, value]) => {
-                      if (key !== "text") {
-                        props += `${key}="${value}"`;
-                      }
-                    });
-                  }
-                  return `<th ${props}>${content}</th>`;
-                })
-                .join("")
-            );
-          } else {
-            console.log(`DO not know how to intepret this data`);
-          }
-        }
-      });
-      console.log({ stringifiedRows });
-      return stringifiedRows;
-    };
-    //TODO add tbody and thead somehow
-    const tableHtml = `
-        <h3>${title}</h3>
-        <table>
-          ${makeRows(rows)
-            .map((row) => `<tr>${row}</tr>`)
-            .join("")}
-        </table>
-      `;
-    console.log({ tableHtml });
-    tableContainer.innerHTML = tableHtml;
-    tableContainer.classList.remove("hidden");
+    buildTable(source);
   } else {
-    const keys = Object.keys(source);
-    const numWords = keys.length;
-    const wordOfTheTab = Math.floor(Math.random() * numWords);
-    colorIndex = (wordOfTheTab % NUM_COLORS) + 1;
-    document.querySelector("#word").innerHTML = keys[wordOfTheTab];
-    document.querySelector("#definition").innerHTML =
-      source[keys[wordOfTheTab]];
-    wordContainer.classList.remove("hidden");
+    buildDictionary(source);
   }
+}
+/**
+ * Gets the data sources from assets
+ * @returns
+ */
+async function getDataSources() {
+  const urls = ["assets/words.json", "assets/tables.json"];
+
+  const sources = [];
+  for (const url of urls) {
+    const data = await fetch(url).catch(() => null);
+    if (data) {
+      const jsonData = await data.json();
+      sources.push(jsonData);
+    }
+  }
+  return sources;
+}
+
+/**
+ * Builds the table given a specific data source
+ */
+function buildTable(source) {
+  const numTables = source.length;
+  const tableOfTheTab = Math.floor(Math.random() * numTables);
+  const colorIndex = (tableOfTheTab % NUM_COLORS) + 1;
+  const { title, rows } = source[tableOfTheTab];
+
+  const makeRows = (dataRows) => {
+    const stringifiedRows = [];
+
+    dataRows.forEach((data) => {
+      if (Array.isArray(data)) {
+        stringifiedRows.push(data.map((d) => `<td>${d}</td>`).join(""));
+      } else {
+        if (data.hasOwnProperty("header")) {
+          stringifiedRows.push(
+            data.header
+              .map((d) => {
+                let content = d;
+                let props = "";
+                if (typeof content !== "string") {
+                  content = d.text;
+                  Object.entries(d).forEach(([key, value]) => {
+                    if (key !== "text") {
+                      props += `${key}="${value}"`;
+                    }
+                  });
+                }
+                return `<th ${props}>${content}</th>`;
+              })
+              .join("")
+          );
+        } else {
+          console.log(`DO not know how to intepret this data`);
+        }
+      }
+    });
+    return stringifiedRows;
+  };
+  //TODO add tbody and thead somehow
+  const tableHtml = `
+      <h3>${title}</h3>
+      <table>
+        ${makeRows(rows)
+          .map((row) => `<tr>${row}</tr>`)
+          .join("")}
+      </table>
+    `;
+  tableContainer.innerHTML = tableHtml;
+  tableContainer.classList.remove("hidden");
+  document.querySelector("#mainContent").classList.add(`pallete_${colorIndex}`);
+}
+/**
+ * Chooses a random word from the source, adds the word and the definition
+ */
+function buildDictionary(source) {
+  const keys = Object.keys(source);
+  const numWords = keys.length;
+  const wordOfTheTab = Math.floor(Math.random() * numWords);
+  const colorIndex = (wordOfTheTab % NUM_COLORS) + 1;
+  document.querySelector("#word").innerHTML = keys[wordOfTheTab];
+  document.querySelector("#definition").innerHTML = source[keys[wordOfTheTab]];
+  wordContainer.classList.remove("hidden");
   document.querySelector("#mainContent").classList.add(`pallete_${colorIndex}`);
 }
